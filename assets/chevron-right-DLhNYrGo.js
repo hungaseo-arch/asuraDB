dc614198f81fce27e498bf259ff0be4a91ef2d92
asura <deviseo@kakao.com>
2026-06-09 13:05:52 +0700

    Updates

--- assets/chevron-right-DLhNYrGo.js
+++ assets/chevron-right-DLhNYrGo.js
@@ -1,6 +1,6 @@
-import{r as e}from"./index-BvYOfKrB.js";/**
+import{r as o}from"./index-kpAqFryf.js";/**
  * @license lucide-vue-next v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const t=e("XIcon",[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]]);export{t as X};
+ */const e=o("ChevronRightIcon",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{e as C};
